da58ea8d80df8f78d0c9123c90478ba58da5b8de
JacksonHates861 <79297883+JacksonHates861@users.noreply.github.com>
2022-02-17 15:46:54 -0600

    Update client.c
    comment noting the changes needed to be made to client.c to allow it to interact with the client.

--- client.c
+++ client.c
@@ -8,7 +8,8 @@
 //so that the client and the server are seperated, and that no data is saved in the client, but
 //passed from the server to the player, and then fromt he player back to the client.
 
-char [] client(struct user crtPlr){
+char [] client(struct user crtPlr){//for all instantces for printf, we need to change them to sendto,
+//and all instances of scanf to recvfrom
 	printf("Usable Letters: %s\n",crtPlr.randomAlphabets);
 	printf("Your Current Score: %d\n",crtPlr.score);
 	printf("Your Opponents Current Score: %d\n",crtPlr.opponentScore);
