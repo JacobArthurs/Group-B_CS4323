45cc875806ba6ba07a0a7408be77b965a8b92d26
JacobArthurs <Arthursjacob01@gmail.com>
2022-02-21 22:17:35 -0600

    Updated main.c

--- main.c
+++ main.c
@@ -5,11 +5,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "player.c"
 
 int main(){//for all instantces for printf, we need to change them to sendto,
 //and all instances of scanf to recvfrom
+
+//Why? Can't we just print to users screen and get information submited from user within main.c and player.c? 
+//We can just add said information to the struct to be sent back and forth between the server.
+//Also, please talk on groupme if you think there needs to be changes. I didn't see this for a long time. - Jacob
     int userSelection;
     printf("1. Single-Player\n");
     printf("2. Multi-Player\n");
@@ -26,5 +31,9 @@ int main(){//for all instantces for printf, we need to change them to sendto,
         printf("Thank you for playing!");
         exit(0);//need to replace exit with a call to terminate the
         //specific socket that this client is addressed to, and only that one.
+
+        //This main.c file is a local players instance of the game. Exiting this instance of the game will not effect
+        //the other players instance of the game. Whenever the local player is in this main menu a socket shouldn't even 
+        //be opened yet because the game hasn't been started yet. - Jacob
     }
 }
