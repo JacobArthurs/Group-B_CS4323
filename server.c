843cd2faab07c9eaf4a9544e31b11c782d1d90b8
JacksonHates861 <79297883+JacksonHates861@users.noreply.github.com>
2022-03-03 15:28:04 -0600

    Update server.c

--- server.c
+++ server.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "player.c"
+#include "word_processing.c"
 
 void server(struct user plr, struct wordList validWords){
     //Allocate array to hold 10 file names
