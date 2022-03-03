46b9d5c0339aaa4fd114b9a3ec61f72d778644a4
JacksonHates861 <79297883+JacksonHates861@users.noreply.github.com>
2022-03-03 14:51:45 -0600

    Update scoreboard.c

--- scoreboard.c
+++ scoreboard.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "player.c"
 
 void singlePlayerScoreboard(struct user currentUser){
     //get index of insertion for currentuser
