c4fd638414ce734e86ecc4da98d3e4cd1fd4712c
JacksonHates861 <79297883+JacksonHates861@users.noreply.github.com>
2022-03-03 14:49:11 -0600

    Update word_processing.c

--- word_processing.c
+++ word_processing.c
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include "text_processing.h"
+#include "player.c"
 
 typedef struct wordList {
     char    words[50][100];                                     // store all the words used through the game
